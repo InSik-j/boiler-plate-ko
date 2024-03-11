3557d20282f88833b1cab77dd52f2405ffa55726
InSik-j <150884733+InSik-j@users.noreply.github.com>
2024-03-11 18:33:06 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // 글자 수
+
 const userSchema = mongoose.Schema({
     name : {
         type: String,
@@ -30,6 +33,26 @@ const userSchema = mongoose.Schema({
     },
     tokenExp:{
         type: Number
+    }
+})
+
+// 저장하기 전에 할 기능
+userSchema.pre('save', function( next ){
+    var user = this; // this는 userSchema를 가르킴
+
+    if(user.isModified('password')){ // 비밀번호가 변경될 때만 변환
+        // 비밀번호를 암호화 시킨다.
+        // salt 생성 -> salt를 이용해서 비밀번호 암호화 -> saltRounds = 글자 수 
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+
+            bcrypt.hash( user.password, salt, function(err, hash){
+                if(err) return next(err)
+
+                user.password = hash; // 성공 시 암호화된 번호로 변경
+                next()
+            })
+        })
     }
 })
 
